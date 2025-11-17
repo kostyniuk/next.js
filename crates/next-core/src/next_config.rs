f23adfb495455ee36c3ea6ae15c5fdc52be504cf
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-17 20:32:46 +0100

    remove old comment

--- crates/next-core/src/next_config.rs
+++ crates/next-core/src/next_config.rs
@@ -1760,7 +1760,6 @@ impl NextConfig {
         Ok(Vc::cell(
             self.experimental
                 .turbopack_remove_unused_imports
-                // TODO enable by default once stable
                 .unwrap_or(matches!(*mode.await?, NextMode::Build)),
         ))
     }
