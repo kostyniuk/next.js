34530cd70058b89910140a2b00e5590f9de3512e
Tobias Koppers <tobias.koppers@googlemail.com>
2025-11-18 15:31:55 +0100

    Turbopack: add NEXT_TURBOPACK_WRITE_ROUTES_HASHES_MANIFEST to write hashes into manifest

--- crates/napi/src/next_api/project.rs
+++ crates/napi/src/next_api/project.rs
@@ -176,6 +176,9 @@ pub struct NapiProjectOptions {
     /// debugging/profiling purposes.
     pub no_mangling: bool,
 
+    /// Whether to write the route hashes manifest.
+    pub write_routes_hashes_manifest: bool,
+
     /// The version of Node.js that is available/currently running.
     pub current_node_js_version: RcStr,
 }
@@ -220,6 +223,9 @@ pub struct NapiPartialProjectOptions {
 
     /// The browserslist query to use for targeting browsers.
     pub browserslist_query: Option<RcStr>,
+
+    /// Whether to write the route hashes manifest.
+    pub write_routes_hashes_manifest: Option<bool>,
 
     /// When the code is minified, this opts out of the default mangling of
     /// local names for variables, functions etc., which can be useful for
@@ -278,6 +284,7 @@ impl From<NapiProjectOptions> for ProjectOptions {
             preview_props,
             browserslist_query,
             no_mangling,
+            write_routes_hashes_manifest,
             current_node_js_version,
         } = val;
         ProjectOptions {
@@ -293,6 +300,7 @@ impl From<NapiProjectOptions> for ProjectOptions {
             preview_props: preview_props.into(),
             browserslist_query,
             no_mangling,
+            write_routes_hashes_manifest,
             current_node_js_version,
         }
     }
@@ -313,6 +321,7 @@ impl From<NapiPartialProjectOptions> for PartialProjectOptions {
             preview_props,
             browserslist_query,
             no_mangling,
+            write_routes_hashes_manifest,
         } = val;
         PartialProjectOptions {
             root_path,
@@ -327,6 +336,7 @@ impl From<NapiPartialProjectOptions> for PartialProjectOptions {
             preview_props: preview_props.map(|props| props.into()),
             browserslist_query,
             no_mangling,
+            write_routes_hashes_manifest,
         }
     }
 }
